672304cab81415dad456664cf6c150b9fc1119a7
Kelly Mahlangu <kellymahlangu01@gmail.com>
2023-09-09 13:08:42 +0200

    betty checks

--- main.c
+++ main.c
@@ -1,29 +1,33 @@
 #include "main.h"
 /**
- * 
+ * main - program entry point
+ * @argc: number of arguments passed
+ * @argv: arguments passed
+ *
+ * Return: 0
 */
 int main(int argc, char *argv[])
 {
-    char input[MAX_INPUT_SIZE];
-    char *args[] = { "hello", NULL};
+	char input[MAX_INPUT_SIZE];
+	char *args[] = { "hello", NULL};
 
-    while (1)
-    {
-        scanf("$ %s", input);
-        
-        if (strcmp(input, "exit") == 0)
-        {
-            return (0);
-        }
-        else if (strcmp(input, "/bin/ls") == 0 || strcmp(input, "ls") == 0)
-        {
-            args[0] = input;
-            execvp(args[0], args);
-        }
-        else
-        {
-            printf("%s: No such file or directory\n", argv[0]);
-        }
-    }
-    return(0);
+	while (1)
+	{
+		scanf("$ %s", input);
+
+		if (strcmp(input, "exit") == 0)
+		{
+			return (0);
+		}
+		else if (strcmp(input, "/bin/ls") == 0 || strcmp(input, "ls") == 0)
+		{
+			args[0] = input;
+			execvp(args[0], args);
+		}
+		else
+		{
+			printf("%s: No such file or directory\n", argv[0]);
+		}
+	}
+	return (0);
 }
